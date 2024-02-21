9b4446ff1b48b0a9d478af2f698187fbdd960423
Tatiana Stewart <tstwrt7@gmail.com>
2024-02-21 16:08:27 -0500

    Wrote tests for endpoints

--- index.test.js
+++ index.test.js
@@ -27,5 +27,23 @@ describe('./musicians endpoint', () => {
         const responseData = JSON.parse(response.text);
 
         expect(responseData.name).toBe('Mick Jagger');
+    });
+    test('endpoint adds new musician', async () => {
+        const response = await request(app).post('/musicians').send({name: 'Westside Gunn', instrument: 'Voice'});
+        const responseData = JSON.parse(response.text);
+
+        expect(responseData.name).toBe('Westside Gunn');
+    });
+    test('endpoint updates specified musician', async () => {
+        const response = await request(app).put('/musicians/3').send({name: 'Erykah Badu'});
+        const responseData = JSON.parse(response.text);
+
+        expect(responseData.name).toBe('Erykah Badu');
+    });
+    test('endpoint deletes specified musician', async () => {
+        const response = await request(app).delete('/musicians/3');
+        const responseData = JSON.parse(response.text);
+
+        expect(responseData.name).toBe(undefined);
     })
 });
